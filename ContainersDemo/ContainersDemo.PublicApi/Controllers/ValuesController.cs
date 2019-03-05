eb02acd334a88fcb695aa3b60fcea38626111429
Paweł Ruciński <meanin@wp.pl>
2019-03-05 13:06:11 +0100

    compose example

---
+++ ContainersDemo/ContainersDemo.PublicApi/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContainersDemo.PublicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly HttpClient _client;
+
+        public ValuesController(HttpClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            _client.BaseAddress = new Uri($"http://{Environment.GetEnvironmentVariable("privateapi")}");
+            var response = await _client.GetAsync("api/values");
+            var str = await response.Content.ReadAsStringAsync();
+            return str;
+        }
+    }
+}
+
